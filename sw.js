a87a7efe1c9a628353646b3b1eb2aab59c4f3d0f
h1dd3n-dev <hm412489@gmail.com>
2026-09-24 10:58:01 +0600

    Update cache name to dms-pwa-cache-v5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker for Deed Management System (PWA)
-const CACHE_NAME = 'dms-pwa-cache-v4';
+const CACHE_NAME = 'dms-pwa-cache-v5';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
